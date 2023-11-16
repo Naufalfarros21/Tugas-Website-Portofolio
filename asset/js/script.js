ac61af8891ff3b95a8b7a64575cab88e66c763d2
Naufal Farros <ofa252116@gmail.com>
2023-11-16 19:51:41 +0700

    modified:   asset/js/script.js 	modified:   asset/styles/style.css 	modified:   index.html

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,3 @@
-/*============ hamburger icon navbar =============*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
 /*============ scroll section active link =============*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
